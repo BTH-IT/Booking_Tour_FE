99522cb3d96db31b3c9875874dbf93ceed9b1cdb
BTH-IT <hungbienthanh@gmail.com>
2023-09-24 22:28:27 +0700

    fix slider and responsive

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,43 +1,7 @@
-import React, { Component } from 'react';
-import Slider from 'react-slick';
+import React from 'react';
 
-class SearchPage extends Component {
-  render() {
-    const settings = {
-      dots: true,
-      className: 'center',
-      centerMode: true,
-      infinite: true,
-      centerPadding: '60px',
-      slidesToShow: 3,
-      speed: 500,
-    };
-    return (
-      <div>
-        <h2>Center Mode</h2>
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-        </Slider>
-      </div>
-    );
-  }
-}
+const SearchPage = () => {
+  return <div>SearchPage</div>;
+};
 
 export default SearchPage;
